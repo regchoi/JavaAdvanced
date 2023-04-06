cd546005cadd5669e77fb2abe908d10a036ff7e7
regchoi <tjrchldyd@gmail.com>
2023-04-06 17:48:34 +0900

    템플릿 메서드 패턴4
    - 템플릿을 호출하여 핵심기능과 분리

---
+++ src/main/java/hello/advanced/app/v4/OrderControllerV4.java
@@ -0,0 +1,29 @@
+package hello.advanced.app.v4;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV4 {
+
+    private final OrderServiceV4 orderServiceV4;
+    private final LogTrace trace;   //Bean에 등록되어있는 해당 인터페이스를 상속받은 생성자로 호출함
+
+    //생성자 필드에 값이 들어가서 파라미터를 전달하지 않고 사용. 파라미터를 넘겨서 사용시 변경할 때 매우 복잡(-> 파라미터를 넘기는 모든 부분을 수정해야해서)
+    @GetMapping("/v4/request")
+    public String request(String itemId) {
+        AbstractTemplate<String> template = new AbstractTemplate<String>(trace) {
+            @Override
+            protected String call() {
+                orderServiceV4.orderItem(itemId);
+                return "ok";
+            }
+        };
+        return template.execute("OrderController.request()");
+    }
+}
